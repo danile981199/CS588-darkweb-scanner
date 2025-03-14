a8768ea73b47aff98b4850b192e575b922a55039
mcsec949 <mcsec949@gmail.com>
2025-03-13 20:43:37 -0400

    Update createTestData.py

--- testdata/createTestData.py
+++ testdata/createTestData.py
@@ -1,3 +1,7 @@
+# This is a harness program that calls functions from the individual random data module
+# It is run from the command line takes a CL input for the number of data items to produce
+# The data produced is written to the screen and to a text file
+
 
 import sys
 import ranames
@@ -30,25 +34,29 @@ def createFullSet():
 
 def createNameCCN():
      # Just names and Credit Card numbers
+     # The credit card numbers include a CVN
+     # They are a mix of random MC/VISA and American Express numbers
      data_item = ""
      data_item = ranames.create_names(1)[0]
-     data_item += ' - '
+     data_item += '  '
      data_item += ranCCN.genRanCCN()
      return data_item
 
 def createNameSSNDob():
+    # Produces a name, an invalid Social Security Number and a random date of birth
     data_item = ""
     data_item = ranames.create_names(1)[0]
-    data_item += ' - '
+    data_item += '  '
     data_item += ranSSN.generate_invalid_ssn()
-    data_item += ' - '
+    data_item += '  '
     data_item += ranDate.generate_random_date()
     return data_item
 
 def createEmalPW():
+    # Produces a random email address and a random password
     data_item = ""
     data_item += ranEmail.genRanEmailAddr()
-    data_item += ' - '
+    data_item += '  '
     data_item += ranEmail.genPassword()
     return data_item
 
@@ -56,7 +64,7 @@ def main():
 
     data_set = []
     m = 15
-    fname = "test_data.txt"
+    fname = "test_data_full.txt"
     adrfile = open(fname,'a') 
 
     if len(sys.argv) > 1:
@@ -67,10 +75,10 @@ def main():
             print("using default of 5\n")
 
     for _ in range(m):
-        #target = createFullSet()
+        target = createFullSet()
         #target = createNameCCN()
         #target = createNameSSNDob()
-        target = createEmalPW()
+        #target = createEmalPW()
         print(target)
         adrfile.write(target + '\n')
 
